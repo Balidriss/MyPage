c3030d4db748cb2f72ec03b2ea43e138f9ccd98a
Idriss Balijon <I.balijon@gmail.com>
2024-02-12 10:45:40 +0100

    start logic for perspective transitioning slide

--- scripts/js/interactions.js
+++ scripts/js/interactions.js
@@ -1,9 +1,36 @@
 "use strict";
 window.addEventListener("guessesPopulated", () => {
-    setZIndex(guesses);
+    setNewIndexOrder(guesses, 0);
 });
-function setZIndex(guesses) {
-    for (let i = 0; i < guesses.length; i++)
-        guesses[i].formElement.classList.add("form-position-" + i.toString());
+if (document.querySelector('button-left') != null) {
+    const sliderLeft = document.querySelector('button-left');
+}
+else {
+    console.error("couldn't find slider left");
+}
+if (document.querySelector('button-right') != null) {
+    const sliderLeft = document.querySelector('button-right');
+}
+else {
+    console.error("couldn't find slider right");
+}
+function setNewIndexOrder(guesses, i) {
+    let y = 0;
+    while (y < guesses.length) {
+        console.log('i : ', i, '...  y : ', y);
+        if (!(i < guesses.length)) {
+            i = 0;
+        }
+        if (y === 0) {
+            guesses[i].formElement.classList.remove("form-position-" + (guesses.length - 1).toString());
+        }
+        else {
+            guesses[i].formElement.classList.remove("form-position-" + (y - 1).toString());
+        }
+        guesses[i].formElement.classList.add("form-position-" + y.toString());
+        i++;
+        y++;
+    }
+    return i;
 }
 ;
--- scripts/ts/interactions.ts
+++ scripts/ts/interactions.ts
@@ -1,14 +1,43 @@
 
 window.addEventListener("guessesPopulated", () => {
-    setZIndex(guesses);
+    setNewIndexOrder(guesses, 0);
 
 
 }
 );
+if (document.querySelector('button-left') != null) {
+    const sliderLeft = document.querySelector('button-left');
+} else {
+    console.error("couldn't find slider left");
+}
+if (document.querySelector('button-right') != null) {
+    const sliderLeft = document.querySelector('button-right');
+} else {
+    console.error("couldn't find slider right");
+}
 
-function setZIndex(guesses: Guesses[]) {
-    for (let i: number = 0; i < guesses.length; i++)
-        guesses[i].formElement.classList.add("form-position-" + i.toString());
 
+
+
+
+function setNewIndexOrder(guesses: Guesses[], i: number): number {
+    let y = 0;
+    while (y < guesses.length) {
+        console.log('i : ', i, '...  y : ', y);
+        if (!(i < guesses.length)) {
+            i = 0;
+        }
+        if (y === 0) {
+            guesses[i].formElement.classList.remove("form-position-" + (guesses.length - 1).toString());
+        } else {
+            guesses[i].formElement.classList.remove("form-position-" + (y - 1).toString());
+
+        }
+        guesses[i].formElement.classList.add("form-position-" + y.toString());
+
+        i++;
+        y++;
+    }
+    return i;
 };
 
