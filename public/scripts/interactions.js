17c5fd3a9172ed1cfcf7d3775f0eb1f54c30dc11
Idriss Balijon <I.balijon@gmail.com>
2024-02-16 10:25:17 +0100

    perspective change click right slide

--- public/scripts/interactions.js
+++ public/scripts/interactions.js
@@ -2,24 +2,55 @@
 window.addEventListener("guessesPopulated", () => {
     setNewIndexOrder(guesses, 0);
 });
+let currentIndex = 0;
 window.addEventListener("load", () => {
     if (document.querySelector('.button-left') != null) {
         const sliderLeft = document.querySelector('.button-left');
+        sliderLeft === null || sliderLeft === void 0 ? void 0 : sliderLeft.addEventListener('click', () => {
+            currentIndex = slide(false, currentIndex);
+        });
     }
     else {
         console.error("couldn't find slider left");
     }
     if (document.querySelector('.button-right') != null) {
-        const sliderLeft = document.querySelector('.button-right');
+        const sliderRight = document.querySelector('.button-right');
+        sliderRight === null || sliderRight === void 0 ? void 0 : sliderRight.addEventListener('click', () => {
+            currentIndex = slide(true, currentIndex);
+        });
     }
     else {
         console.error("couldn't find slider right");
     }
 });
-function setNewIndexOrder(guesses, i) {
+function slide(isRight, currentIndex) {
+    console.log(currentIndex);
+    if (isRight) {
+        currentIndex--;
+        //if currentindex go beyond 0 that means we have to loop to the end
+        if ((currentIndex < 0)) {
+            // TODO
+            currentIndex = guesses.length - 1;
+        }
+        setNewIndexOrder(guesses, currentIndex);
+    }
+    else {
+        currentIndex++;
+        //if currentindex go beyond the length that means we have to loop to the start
+        if (!(currentIndex < guesses.length)) {
+            // TODO
+            currentIndex = 0;
+        }
+        setNewIndexOrder(guesses, currentIndex);
+    }
+    return currentIndex;
+}
+function setNewIndexOrder(guesses, currentIndex) {
     let y = 0;
+    let i = currentIndex;
     while (y < guesses.length) {
         console.log('i : ', i, '...  y : ', y);
+        console.log('i < guesses.length : ', (i < guesses.length));
         if (!(i < guesses.length)) {
             i = 0;
         }
@@ -33,6 +64,5 @@ function setNewIndexOrder(guesses, i) {
         i++;
         y++;
     }
-    return i;
 }
 ;
--- scripts/ts/interactions.ts
+++ scripts/ts/interactions.ts
@@ -1,35 +1,72 @@
 
+
 window.addEventListener("guessesPopulated", () => {
+
     setNewIndexOrder(guesses, 0);
 
 
 }
 );
-
+let currentIndex = 0;
 window.addEventListener("load", () => {
 
     if (document.querySelector('.button-left') != null) {
         const sliderLeft = document.querySelector('.button-left');
+        sliderLeft?.addEventListener('click', () => {
+
+            currentIndex = slide(false, currentIndex);
+
+        })
     } else {
         console.error("couldn't find slider left");
     }
     if (document.querySelector('.button-right') != null) {
-        const sliderLeft = document.querySelector('.button-right');
+        const sliderRight = document.querySelector('.button-right');
+        sliderRight?.addEventListener('click', () => {
+
+            currentIndex = slide(true, currentIndex);
+        })
     } else {
         console.error("couldn't find slider right");
     }
 });
 
 
+function slide(isRight: boolean, currentIndex: number): number {
+
+    console.log(currentIndex);
+    if (isRight) {
+        currentIndex--;
+        //if currentindex go beyond 0 that means we have to loop to the end
+        if ((currentIndex < 0)) {
+            // TODO
+            currentIndex = guesses.length - 1;
+        }
+        setNewIndexOrder(guesses, currentIndex);
+    }
+    else {
+        currentIndex++;
+
+        //if currentindex go beyond the length that means we have to loop to the start
+        if (!(currentIndex < guesses.length)) {
+            // TODO
+            currentIndex = 0;
+        }
+        setNewIndexOrder(guesses, currentIndex);
+    }
+    return currentIndex;
+}
 
 
 
-
-function setNewIndexOrder(guesses: Guesses[], i: number): number {
+function setNewIndexOrder(guesses: Guesses[], currentIndex: number) {
     let y = 0;
+    let i = currentIndex;
     while (y < guesses.length) {
         console.log('i : ', i, '...  y : ', y);
+        console.log('i < guesses.length : ', (i < guesses.length));
         if (!(i < guesses.length)) {
+
             i = 0;
         }
         if (y === 0) {
@@ -42,7 +79,8 @@ function setNewIndexOrder(guesses: Guesses[], i: number): number {
 
         i++;
         y++;
+
     }
-    return i;
+
 };
 
