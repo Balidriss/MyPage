a48dd52e38f92bfc94f8753f7c35cbc75568bc92
Idriss Balijon <I.balijon@gmail.com>
2024-02-25 20:33:38 +0100

    update CV + split cv section

---
+++ public/includes/cv-formations.php
@@ -0,0 +1,47 @@
+                <article id="professions" class="">
+                    <h3>Professions</h3>
+                    <div class="grid-cv">
+                        <div class="date">
+                            Septembre 2023
+                        </div>
+                        <div class="timeline">
+                            <span class="marker"></span>
+                        </div>
+                        <div class="desc-cv">
+                            <h4>Stagiaire développeur Web </h4><h5>Drosalys -
+                                Clermont-Ferrand (63)</h5>
+                            <p> Stage de finalisation de formation Human Booster, réalisation de la partie front-end
+                                wordpress à partir d'une maquette à l'aide de Champ ACF, Symfony, SCSS et typescript.
+                            </p>
+                        </div>
+                        <div class="date">
+                            Intérims
+                        </div>
+                        <div class="timeline">
+                            <span class="marker"></span>
+                        </div>
+                        <div class="desc-cv">
+                            <p>Contrôleur qualité</p>
+                            <p>Manutentionnaire</p>
+                            <p>Conducteur transpalette</p>
+                            <p>Preparateur de commande</p>
+                            <p>Conducteur de ligne</p>
+                        </div>
+                        <div class="date">Juillet 2012 -</br>Septembre 2011 </div>
+                        <div class="timeline">
+                            <span class="marker"></span>
+                        </div>
+                        <div class="desc-cv">
+                            <h4>Apprenti Ingénieur Procédés</h4><h5>Hologram Industries - Bussy Saint-Georges (77)</h5>
+                            <p> 1an en alternance - Identifier des améliorations de productivité, proposer des solutions d'automate pour la détection de défauts, rédaction de mode opératoire, communications et rencontre d'entreprise optronique européenne</p>
+                        </div>
+                        <div class="date">Juin 2011 -</br>Septembre 2009</div>
+                        <div class="timeline">
+                            <span class="marker"></span>
+                        </div>
+                        <div class="desc-cv">
+                            <h4>Assistant ingénieur optique et imagerie</h4> <h5>KIS Photo-Me, Echirolles (38)</h5>
+                            <p>Réalisation d'un objectif de développeuse de bobine de cinéma et dessin industriel.</p>
+                        </div>
+                    </div>
+                </article>
--- public/includes/cv-section.php
+++ public/includes/cv-section.php
@@ -5,9 +5,8 @@
                     <h2>Mon parcours</h2>
                 </div>
                 <p>Une perspective unique dans le monde du numérique.</p>
-                <p><em><strong>Curieux et créatif</strong>, je recherche un environnement stimulant pour accroître mes talents de développeur web et participer à la réalisation de projets.
-Je recherche une entreprise pour une alternance en collaboration avec Human Booster du <strong>18 mars 2024 au 12 mars 2025</strong> sur un rythme de 
-3 semaines par mois en entreprise.</em></p>
+                <!-- TO DO : from db -->
+                <p><em><strong>Curieux et créatif</strong>, je recherche un environnement stimulant pour accroître mes talents de développeur web et participer à la réalisation de projets. Je recherche une entreprise pour une alternance en collaboration avec Human Booster du <strong>18 mars 2024 au 12 mars 2025</strong> sur un rythme de 3 semaines par mois en entreprise.</em></p>
                 <div class="download-button-container">
                     <p id="download-description"></p>
                 <a href="public/download/CV_Dev_Web_Alternance_Balijon_2024 _ 18Mars2024-12Mars2025(numerique&print).pdf" class="cta" download>Télécharger</a></div>
@@ -26,134 +25,12 @@ Je recherche une entreprise pour une alternance en collaboration avec Human Boos
                     </div>
                     <div class="tab-cv projects">
                         <h3>Travaux personnels</h3>
-                        <p><strong>Fevriers</strong> </br> 2024</p>
+                        <p><strong>Fevriers</strong></br>2024</p>
                     </div>
                 </div>
-                <article id="formations" class="show">
-                    <h3>Formations</h3>
-                    <div class="grid-cv">
-                        <div class="date">
-                            Janvier 2024
-                        </div>
-                        <div class="timeline">
-                            <span class="marker"></span>
-                        </div>
-                        <div class="desc-cv">
-                            <h4>Implémentez vos bases de données relationnelles avec SQL </br><i>Openclassroom - Autoformation</i></h4>
-                            <p>Rappels sur les requêtes SQL.
-                            </p>
-                            <h4>Concevez votre site web avec PHP et MySQL </br><i>Openclassroom - Autoformation</i></h4>
-                            <p>Formation PHP, écoute de requete,système de connexion, sessions, cookies, requètes SQL,
-                                partage de recette entre utilisateur avec système de notes et commentaires.
-                            </p>
-                            <h4>Créez votre site web avec HTML5 et CSS3 </br><i>Openclassroom - Autoformation</i></h4>
-                            <p>Formation HTML CSS plus détaillé.</p>
-
-                        </div>
-                        <div class="date">
-                            Décembre 2023
-                        </div>
-                        <div class="timeline">
-                            <span class="marker"></span>
-                        </div>
-                        <div class="desc-cv">
-                            <h4>Manage Your Code Project With Git and GitHub </br><i>Openclassroom - Autoformation</i></h4>
-                            <p>Formation en anglais GitBash, commit, branch, push, merge , corrections.
-                            </p>
-                            <h4>Write JavaScript for the Web </br><i>Openclassroom - Autoformation</i></h4>
-                            <p>Formation en anglais javascript avancé, manipulation du DOM, requête de données,
-                                programation asynchrone.</p>
-                            <h4>Learn Programming With JavaScript </br><i>Openclassroom - Autoformation</i></h4>
-                            <p>Formation en anglais javascript , les bases du langage javascript.</p>
-                            <h4>Build Your First Web Pages With HTML and CSS </br><i>Openclassroom - Autoformation</i></h4>
-                            <p>Formation en anglais HTML CSS , les bases dans la création d'une page web.</p>
-                        </div>
-                        <div class="date">
-                            02 Octobre 2023 -</br> 31 Juin 2023
-                        </div>
-
-                        <div class="timeline">
-                            <span class="marker"></span>
-                        </div>
-                        <div class="desc-cv">
-                            <h4>ICDL marketing numérique </br> <i>Human Booster -
-                                Clermont-Ferrand (63)</i></h4>
-                            <p> Une formation complète incluant marketing, SEO, design
-                                UI/UX et programation HTML CSS JS.</p>
-                        </div>
-                        <div class="date">Juillet 2012 -</br>Septembre 2011 </div>
-                        <div class="timeline">
-                            <span class="marker"></span>
-                        </div>
-
-                        <div class="desc-cv">
-                            <h4>Ingénieur optique et vision industrielle</br><i>Télécom Saint-Étienne - Saint-Étienne (42)
-</i></h4>
-                            <p> 1an en alternance - Electronique, mécanique, métrologie, informatique, programation c++,
-                                automatique, traitement d'image, optique
-                                géométrique et ondulatoire,
-                                rédaction de cahier des charges.</p>
-                        </div>
-
-
-                        <div class="date">Juin 2011 -</br>Septembre 2009</div>
-                        <div class="timeline">
-                            <span class="marker"></span>
-                        </div>
-                        <div class="desc-cv">
-                            <h4>BTS Génie optique photonique,</br><i>André Argouges - Grenoble (38)</i></h4>
-                            <p>Formation technique dans l'optique géométrique et ondulatoire, métrologie,théorèmes
-                                physiques et mécaniques.</p>
-                        </div>
-                    </div>
-                </article>
-                <article id="professions" class="">
-                    <h3>Professions</h3>
-                    <div class="grid-cv">
-                        <div class="date">
-                            29 Septembre 2023 -</br>13 Septembre 2023
-                        </div>
-                        <div class="timeline">
-                            <span class="marker"></span>
-                        </div>
-                        <div class="desc-cv">
-                            <h4>Stagiaire développeur Web </br><i>Drosalys -
-                                Clermont-Ferrand (63)</i></h4>
-                            <p> Stage de finalisation de formation Human Booster, réalisation de la partie front-end
-                                wordpress à partir d'une maquette à l'aide de Champ ACF, Symfony, SCSS et typescript.
-                            </p>
-                        </div>
-                        <div class="date">Juillet 2012 -</br>Septembre 2011 </div>
-                        <div class="timeline">
-                            <span class="marker"></span>
-                        </div>
-
-                        <div class="desc-cv">
-                            <h4>Ingénieur optique et vision industrielle</br><i>Télécom Saint-Étienne - Saint-Étienne (42)
-</i></h4>
-                            <p> 1an en alternance - Electronique, mécanique, métrologie, informatique, programation c++,
-                                automatique, traitement d'image, optique
-                                géométrique et ondulatoire,
-                                rédaction de cahier des charges.</p>
-                        </div>
-
-
-                        <div class="date">Juin 2011 -</br>Septembre 2009</div>
-                        <div class="timeline">
-                            <span class="marker"></span>
-                        </div>
-                        <div class="desc-cv">
-                            <h4>BTS Génie optique photonique,</br> <i>André Argouges - Grenoble (38)</i></h4>
-                            <p>Formation technique dans l'optique géométrique et ondulatoire, métrologie,théorèmes
-                                physiques et mécaniques.</p>
-                        </div>
-                    </div>
-
-                </article>
-                <article id="projects" class="">
-
-
-                </article>
-            </div>
+                <!-- TO DO : from db -->
+                <?php require_once(__DIR__ . '/cv-formations.php'); ?>
+                <?php require_once(__DIR__ . '/cv-professions.php'); ?>
+                <?php require_once(__DIR__ . '/cv-projects.php'); ?>
             <div class=""></div>
         </section>
