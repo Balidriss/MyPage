512e97c53c939e874a6ec000ebb755571bc1e072
Idriss Balijon <I.balijon@gmail.com>
2024-02-25 13:42:56 +0100

    update for alterance + download

--- public/includes/cv-section.php
+++ public/includes/cv-section.php
@@ -5,7 +5,12 @@
                     <h2>Mon parcours</h2>
                 </div>
                 <p>Une perspective unique dans le monde du numérique.</p>
-                <a href="" class="cta">Télécharger</a>
+                <p><em><strong>Curieux et créatif</strong>, je recherche un environnement stimulant pour accroître mes talents de développeur web et participer à la réalisation de projets.
+Je recherche une entreprise pour une alternance en collaboration avec Human Booster du <strong>18 mars 2024 au 12 mars 2025</strong> sur un rythme de 
+3 semaines par mois en entreprise.</em></p>
+                <div class="download-button-container">
+                    <p id="download-description"></p>
+                <a href="public/download/CV_Dev_Web_Alternance_Balijon_2024 _ 18Mars2024-12Mars2025(numerique&print).pdf" class="cta" download>Télécharger</a></div>
             </article>
             <div class="cv-container">
                 <div class="tabs-cv">
