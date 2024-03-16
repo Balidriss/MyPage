1f4a395c5560c9527a22f63e5ab2448261ae7f31
Idriss Balijon <I.balijon@gmail.com>
2024-03-16 14:12:43 +0100

    fixe width cv tabs

--- views/partials/Home/CV/cv-projects.php
+++ views/partials/Home/CV/cv-projects.php
@@ -1,54 +1,59 @@
 <article id="projects" class="">
-                <h3>Projets</h3>
-                    <div class="grid-cv">
-                        <div class="date">
-                           Actuellement - Fevriers 2024
-                        </div>
-                        <div class="timeline">
-                            <span class="marker"></span>
-                        </div>
-                        <div class="desc-cv">
-                        <h4>IB Page</h4><h5>Site personel en ligne - Autoformation</h5>
-                            <p>Réalisation d'un site utilisant PHP et MySQL.</p>
-                            <p>Objectifs : Créer un back office permettant de modifier les parties du site pouvant évoluer, ainsi que de recevoir les formulaires de contact.
-                            </p>
-                        </div>
-                        <div class="date">
-                           Janvier 2024
-                        </div>
-                        <div class="timeline">
-                            <span class="marker"></span>
-                        </div>
-                        <div class="desc-cv">
-                            <h4><a href ="https://balidriss.github.io/Salon-Du-Vin_Web-Statique/" class="link-out">Salon Du Vin (Fictif)</a></h4><h5>Projet Technique de séléction - <a href="https://humanbooster.com/">Human Booster</a> x <a href="https://www.coqpit.fr/">Coqpit</a></h5>
-                            <p>Réalisation d'une page statique à partir d'une maquette Figma</p>
-                            <p>Consigne : Être le plus fidèle possible à la maquette en utilisant uniquement HTML et CSS. Il n'est pas nécessaire que ce soit responsive. Bonus pour l'utilisation de JavaScript pour les carrousels. 
-                            </p>
-                            <div>
-                            <h4><a href ="https://gitlab.com/ibalijon/Your_Pizza_Recipes_PHP_Training" class="link-out">Your Pizza (Fictif)</a></h4><h5>Partage de recette de pizza - <a href="https://openclassrooms.com/">Autoformation - Openclassroom</a></h5>
-                            <p>Réalisation en local d'un site de partage de recette de pizza en suivant le fil rouge d'Openclassroom sur le cours de PHP et requete SQL</p>
-                            <p>Objectif : faire un système d'identification d'utilisateur. Donner la possibilité de création de contenu par l'utilisateur (ajout, suppression et édition). Créer un système d'ajout de commentaire et d'évaluation de la recette.
-                            </p>
-                            </div>
-                            <div>
-                            <h4><a href ="https://balidriss.github.io/Site-Photo-Fictif-HTML5-CSS3/" class="link-out">Robbie Lens Photographie (Fictif)</a></h4><h5>Site Vitrine Photographe - <a href="https://openclassrooms.com/">Autoformation - Openclassroom</a></h5>
-                            <p>Réalisation d'un site vitrine en suivant le fil rouge d'Openclassroom sur le cours de HTML5 CSS3</p>
-                            <p>Objectif : Faire un site vitrine responsive.
-                            </p>
-                            </div>
-                        </div>
-                        <div class="date">
-                            Janvier 2024 - Juillet 2023
-                        </div>
-                        <div class="timeline">
-                            <span class="marker"></span>
-                        </div>
-                        <div class="desc-cv">
-                        <h4><a href ="https://balidriss.github.io/SITE/" class="link-out">Mon Site</a></h4><h5>Site personel - Autoformation</h5>
-                            <p>Réalisation d'un site utilisant HTML CSS JS.</p>
-                            <p>Objectifs : apprendre à créer une page responsive avec du contenus anglais et français.
-                            </p>
-                        </div>
-                    </div>
-                </article>
+    <h3>Travaux</h3>
+    <div class="grid-cv">
+        <div class="date">
+            Actuellement - Fevriers 2024
+        </div>
+        <div class="timeline">
+            <span class="marker"></span>
+        </div>
+        <div class="desc-cv">
+            <h4>IB Page</h4>
+            <h5>Site personel en ligne - Autoformation</h5>
+            <p>Réalisation d'un site utilisant PHP et MySQL.</p>
+            <p>Objectifs : Créer un back office permettant de modifier les parties du site pouvant évoluer, ainsi que de recevoir les formulaires de contact.
+            </p>
+        </div>
+        <div class="date">
+            Janvier 2024
+        </div>
+        <div class="timeline">
+            <span class="marker"></span>
+        </div>
+        <div class="desc-cv">
+            <h4><a href="https://balidriss.github.io/Salon-Du-Vin_Web-Statique/" class="link-out">Salon Du Vin (Fictif)</a></h4>
+            <h5>Projet Technique de séléction - <a href="https://humanbooster.com/">Human Booster</a> x <a href="https://www.coqpit.fr/">Coqpit</a></h5>
+            <p>Réalisation d'une page statique à partir d'une maquette Figma</p>
+            <p>Consigne : Être le plus fidèle possible à la maquette en utilisant uniquement HTML et CSS. Il n'est pas nécessaire que ce soit responsive. Bonus pour l'utilisation de JavaScript pour les carrousels.
+            </p>
+            <div>
+                <h4><a href="https://gitlab.com/ibalijon/Your_Pizza_Recipes_PHP_Training" class="link-out">Your Pizza (Fictif)</a></h4>
+                <h5>Partage de recette de pizza - <a href="https://openclassrooms.com/">Autoformation - Openclassroom</a></h5>
+                <p>Réalisation en local d'un site de partage de recette de pizza en suivant le fil rouge d'Openclassroom sur le cours de PHP et requete SQL</p>
+                <p>Objectif : faire un système d'identification d'utilisateur. Donner la possibilité de création de contenu par l'utilisateur (ajout, suppression et édition). Créer un système d'ajout de commentaire et d'évaluation de la recette.
+                </p>
             </div>
+            <div>
+                <h4><a href="https://balidriss.github.io/Site-Photo-Fictif-HTML5-CSS3/" class="link-out">Robbie Lens Photographie (Fictif)</a></h4>
+                <h5>Site Vitrine Photographe - <a href="https://openclassrooms.com/">Autoformation - Openclassroom</a></h5>
+                <p>Réalisation d'un site vitrine en suivant le fil rouge d'Openclassroom sur le cours de HTML5 CSS3</p>
+                <p>Objectif : Faire un site vitrine responsive.
+                </p>
+            </div>
+        </div>
+        <div class="date">
+            Janvier 2024 - Juillet 2023
+        </div>
+        <div class="timeline">
+            <span class="marker"></span>
+        </div>
+        <div class="desc-cv">
+            <h4><a href="https://balidriss.github.io/SITE/" class="link-out">Mon Site</a></h4>
+            <h5>Site personel - Autoformation</h5>
+            <p>Réalisation d'un site utilisant HTML CSS JS.</p>
+            <p>Objectifs : apprendre à créer une page responsive avec du contenus anglais et français.
+            </p>
+        </div>
+    </div>
+</article>
+</div>
--- views/partials/Home/CV/cv-section.php
+++ views/partials/Home/CV/cv-section.php
@@ -1,36 +1,36 @@
 <section id="cv-section">
-            <article class="main-text-section">
-                <div class="title">
-                    <p>CV</p>
-                    <h2>Mon parcours</h2>
-                </div>
-                <p>Une perspective unique dans le monde du numérique.</p>
-                <!-- TO DO : from db -->
-                <p><em><strong>Curieux et créatif</strong>, je recherche un environnement stimulant pour accroître mes talents de développeur web et participer à la réalisation de projets. Je recherche une entreprise pour une alternance en collaboration avec Human Booster du <strong>18 mars 2024 au 12 mars 2025</strong> sur un rythme de 3 semaines par mois en entreprise.</em></p>
-                <div class="download-button-container">
-                    <p id="download-description"></p>
-                <a href="download/CV_Dev_Web_Alternance_Balijon_2024 _ 18Mars2024-12Mars2025(numerique&print).pdf" class="cta" download>Télécharger</a></div>
-            </article>
-            <div class="cv-container">
-                <div class="tabs-cv">
-                    <div class="tab-cv formations show">
-                        <h3>Formations</h3>
-                        <p><strong>Janvier</strong></br>
-                            2024</p>
-                    </div>
-                    <div class="tab-cv professions">
-                        <h3>Professions</h3>
-                        <p><strong>Septembre</strong></br>
-                            2023</p>
-                    </div>
-                    <div class="tab-cv projects">
-                        <h3>Travaux personnels</h3>
-                        <p><strong>Fevriers</strong></br>2024</p>
-                    </div>
-                </div>
-                <!-- TO DO : from db -->
-                <?php require_once(__DIR__ . '/cv-formations.php'); ?>
-                <?php require_once(__DIR__ . '/cv-professions.php'); ?>
-                <?php require_once(__DIR__ . '/cv-projects.php'); ?>
-            <div class=""></div>
-        </section>
+    <article class="main-text-section">
+        <div class="title">
+            <p>CV</p>
+            <h2>Mon parcours</h2>
+        </div>
+        <p>Une perspective unique dans le monde du numérique.</p>
+        <!-- TO DO : from db -->
+        <p><em><strong>Curieux et créatif</strong>, je recherche un environnement stimulant pour accroître mes talents de développeur web et participer à la réalisation de projets. Je recherche une entreprise pour une alternance en collaboration avec Human Booster du <strong>18 mars 2024 au 12 mars 2025</strong> sur un rythme de 3 semaines par mois en entreprise.</em></p>
+        <div class="download-button-container">
+            <p id="download-description"></p>
+            <a href="download/CV_Dev_Web_Alternance_Balijon_2024 _ 18Mars2024-12Mars2025(numerique&print).pdf" class="cta" download>Télécharger</a>
+        </div>
+    </article>
+    <div class="cv-container">
+        <div class="tabs-cv">
+            <div class="tab-cv formations show">
+                <h3>Formations</h3>
+                <p><strong>Janvier</strong></br>
+                    2024</p>
+            </div>
+            <div class="tab-cv professions">
+                <h3>Professions</h3>
+                <p><strong>Septembre</strong></br>
+                    2023</p>
+            </div>
+            <div class="tab-cv projects">
+                <h3>Travaux</h3>
+                <p><strong>Fevriers</strong></br>2024</p>
+            </div>
+        </div>
+        <!-- TO DO : from db -->
+        <?php require_once(__DIR__ . '/cv-formations.php'); ?>
+        <?php require_once(__DIR__ . '/cv-professions.php'); ?>
+        <?php require_once(__DIR__ . '/cv-projects.php'); ?>
+</section>
