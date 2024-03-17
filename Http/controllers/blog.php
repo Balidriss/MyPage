7e3b432fcdc33d81b877852d0dccd425c7b6cf4d
Idriss Balijon <I.balijon@gmail.com>
2024-03-17 20:46:23 +0100

    add favicon in controller

--- Http/controllers/blog.php
+++ Http/controllers/blog.php
@@ -1,5 +1,6 @@
 <?php
 view("blog.view.php", [
+    'ico' => assetPath('icon', "fav.ico"),
     'style' => assetPath('style', "blog-style.css"),
     'heading' => 'Blog'
 ]);
--- Http/controllers/projects.php
+++ Http/controllers/projects.php
@@ -1,5 +1,6 @@
 <?php
 view("projects.view.php", [
+    'ico' => assetPath('icon', "fav.ico"),
     'style' => assetPath('style', "projects-style.css"),
     'js' => assetPath('script', "projects.js"),
     'heading' => 'Projects'
