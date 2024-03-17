0565a394bc72ef3fba57edb77143d724128ce37b
Idriss Balijon <I.balijon@gmail.com>
2024-03-17 16:37:05 +0100

    add copyrights in footer and change style

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -3,4 +3,6 @@
         <a href="">IB</a>
     </div>
     <div class="socials"><a href="www.linkedin.com/in/idriss-balijon"><img src=<?= assetPath('img', "linkedin.png") ?>></a><a href=""><img src=<?= assetPath('img', "X.svg") ?>></a></div>
+    <p>&copy; 2024 Idriss Balijon. Tous droits réservés.</p>
+    <p>Design inspiré par Alvin Elian</p>
 </footer>
