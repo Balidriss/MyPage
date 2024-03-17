a54e9cf15d952e6dca304e14a1bbaa995d6f6b49
Idriss Balijon <I.balijon@gmail.com>
2024-03-17 20:15:20 +0100

    add fordidden & 404 page

---
+++ views/403.php
@@ -0,0 +1,13 @@
+<?php require base_path('views/partials/head.php'); ?>
+<?php require base_path('views/partials/header.php'); ?>
+<div class="gradient-bg"></div>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <h1>Désolé, vous n'êtes pas authorisé.</h1>
+        <p class="mt-4">
+            <a href="/" class="text-blue-500 underline">Revenir à l'accueil.</a>
+        </p>
+    </div>
+</main>
+
+<?php require base_path('views/partials/footer.php'); ?>
--- views/partials/head.php
+++ views/partials/head.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="fr">
 
+
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="<?= $ico ?>" type="image/x-icon">
+    <link rel="icon" href="<?= $ico ?? assetPath('icon', 'fav.ico') ?>" type="image/x-icon">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
-    <script src="<?= $js ?>" defer></script>
-    <link rel="stylesheet" href="<?= $style ?>" type="text/css" />
-    <title>Idriss Balijon - <?= $heading ?></title>
+    <script src="<?= $js ?? "" ?>" defer></script>
+    <link rel="stylesheet" href="<?= $style ?? assetPath('style', 'home-style.css') ?>" type="text/css" />
+    <title>Idriss Balijon - <?= $heading ?? 'oups' ?></title>
 </head>
