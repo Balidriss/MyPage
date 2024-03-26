d394403959ff557b92298c707debab3e85996265
Idriss Balijon <I.balijon@gmail.com>
2024-03-26 01:55:25 +0100

    anti spam db

--- Http/controllers/guess/processor.php
+++ Http/controllers/guess/processor.php
@@ -1,7 +1,22 @@
 <?php
 $userAttempt = $_POST['attempt'];
 $index = $_POST['index'];
+$time = 1;
 
+
+if (timePassed('processor', time()) >= $time ?? true) {
+    storeTime('processor');
+} else {
+
+    echo json_encode(['error' => "too fast"]);
+    exit();
+}
+
+if (!isset($index) || !isset($userAttempt)) {
+
+    echo json_encode(['error' => 'missing $_POST']);
+    exit();
+}
 $guess = Core\App::resolve(Core\Quiz::class)->find($index);
 
 if ($guess->attempt($userAttempt)) {
