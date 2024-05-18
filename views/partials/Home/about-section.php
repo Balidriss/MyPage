07d6e18a379d7bb516f1bc73611491036ecf52a3
Idriss Balijon <I.balijon@gmail.com>
2024-05-18 16:42:11 +0200

    moved mail & phone from about to footer

--- views/partials/Home/about-section.php
+++ views/partials/Home/about-section.php
@@ -7,10 +7,6 @@
                 <h2>Clermont-Ferrand</h2>
             </div>
         </div>
-        <ul>
-            <li><img src="assets/img/mail.svg" loading="lazy" alt="Envoyez un mail">contact@ibsoft.fr</li>
-            <li><img src="assets/img/call.svg" loading="lazy" alt="Appelez moi">06 XX XX XX XX</li>
-        </ul>
     </article>
 
 </section>
