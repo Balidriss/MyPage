6e9f18102bce0a0685b116fa54b9e6fd5e9510d2
Idriss Balijon <I.balijon@gmail.com>
2024-05-25 13:37:39 +0200

    remove phone number from footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -5,7 +5,6 @@
     <div class="socials"><a href="www.linkedin.com/in/idriss-balijon"><img src=<?= assetPath('img', "linkedin.png") ?> alt="Mon Linkedin"></a><a href=""><img src=<?= assetPath('img', "X.svg") ?> alt="Mon profile X"></a></div>
     <ul>
         <li><img src="assets/img/mail.svg" loading="lazy" alt="Envoyez un mail">contact@ibsoft.fr</li>
-        <li><img src="assets/img/call.svg" loading="lazy" alt="Appelez moi">06 XX XX XX XX</li>
     </ul>
     <p>&copy; 2024 Idriss Balijon. Tous droits réservés.</p>
     <p>Design inspiré de Alvin Elian</p>
