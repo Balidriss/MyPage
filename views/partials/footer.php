736745e9cd50f915d9dabfcddde7e1c9fbf5a9c4
Idriss Balijon <i.balijon@gmail.com>
2024-06-05 11:59:49 +0200

    correct linkedin link

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -2,7 +2,7 @@
     <div class="logo">
         <a href="/">IB</a>
     </div>
-    <div class="socials"><a href="www.linkedin.com/in/idriss-balijon"><img src=<?= assetPath('img', "linkedin.png") ?> alt="Mon Linkedin"></a><a href=""><img src=<?= assetPath('img', "X.svg") ?> alt="Mon profile X"></a></div>
+    <div class="socials"><a href="https://linkedin.com/in/idriss-balijon"><img src=<?= assetPath('img', "linkedin.png") ?> alt="Mon Linkedin"></a><a href=""><img src=<?= assetPath('img', "X.svg") ?> alt="Mon profile X"></a></div>
     <ul>
         <li><img src="assets/img/mail.svg" loading="lazy" alt="Envoyez un mail">contact@ibsoft.fr</li>
     </ul>
