ab8bb4679c0953e53105da96a0ff66746c88ecd6
Idriss Balijon <I.balijon@gmail.com>
2024-06-23 15:35:22 +0200

    basic 404 with random quotes ¯\_(ツ)_/¯

--- Http/404.php
+++ Http/404.php
@@ -1,6 +1,21 @@
 <?php
 
-$apiFirstPart = "Je";
-$apiSecondPart = "suis";
-$apiLastPart = "désolé...";
-$randomMessage = $apiFirstPart . ' ' . $apiSecondPart . ' ' . $apiLastPart;
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://zenquotes.io/api/quotes");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($curl);
+curl_close($curl);
+
+$defaultQuotes = [
+    (object) [
+        "q" => "I should go",
+        "a" => "Commander Shepard",
+    ]
+];
+
+$quotes = json_decode($output) ?? $defaultQuotes;
+$numQuotes = count($quotes);
+$randomIndex = rand(0, $numQuotes - 1);
+
+$randomMessage['quote'] = $quotes[$randomIndex]->q;
+$randomMessage['from'] =  $quotes[$randomIndex]->a;
--- views/404.php
+++ views/404.php
@@ -1,17 +1,13 @@
-<?php require base_path('views/partials/head.php');
-?>
+<?php require base_path('views/partials/head.php'); ?>
 <?php require base_path('Http/404.php'); ?>
-
-<div class="gradient-bg"></div>
 <main class="notfound">
 
     <h1 class="heading">404</h1>
-    <p>Désolé, cette page n'éxiste pas.</p>
-    <p class="random"><?= $randomMessage ?></p>
+    <p class="sorry"><strong>Désolé</strong>, cette page n'éxiste pas.</p>
+    <p class="quote"><?= $randomMessage['quote'] ?></p>
+    <p class="from"><?= $randomMessage['from'] ?> </p>
     <p class="back">
         <a class="" href="/">Revenir à l'accueil.</a>
     </p>
 
 </main>
-
-<?php require base_path('views/partials/footer.php'); ?>
